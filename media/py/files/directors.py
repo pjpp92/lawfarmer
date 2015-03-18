0a79c6c889c13b3c188dd8fc7ea25b5abc211557
pjpp92 <pjpp92@gmail.com>
2015-03-18 17:28:32 +0100

    tiruriru

---
+++ media/py/files/directors.py
@@ -0,0 +1,59 @@
+directors = [
+    {
+        'name': 'Woody Allen',
+        'date_of_birth': 1935,
+        'alive': True
+    },
+
+    {
+        'name': 'Luis Bunuel',
+        'date_of_birth': 1900,
+        'alive': False
+    },
+
+    {
+        'name': 'Alfred Hitchcock',
+        'date_of_birth': 1899,
+        'alive': False,
+    },
+
+    {
+        'name': 'Ingmar Bergman',
+        'date_of_birth': 1918,
+        'alive': False,
+    },
+
+    {
+        'name': 'Steven Spielberg',
+        'date_of_birth': 1946,
+        'alive': True
+    },
+
+    {
+        'name': 'Christopher Nolan',
+        'date_of_birth': 1970,
+        'alive': True
+    },
+
+    {
+        'name': 'Stanley Kubrick',
+        'date_of_birth': 1928,
+        'alive': False
+    },
+
+
+    {
+        'name': 'Charles Chaplin',
+        'date_of_birth': 1889,
+        'alive': False
+    },
+
+    {
+        'name': 'Quentin Tarantino',
+        'date_of_birth': 1963,
+        'alive': True
+    }
+
+]
+
+
---
+++ media/py/files/movies.py
@@ -0,0 +1,137 @@
+movies = [
+    {
+        'title': 'Midnight in Paris',
+        'type': 'comedy',
+        'director': 'Woody Allen',
+        'year_created': 2011
+    },
+
+    {
+        'title': 'Vicky Cristina Barcelona',
+        'type': 'comedy',
+        'director': 'Woody Allen',
+        'year_created': 2008
+    },
+
+    {
+        'title': 'Vicky Cristina Barcelona',
+        'type': 'comedy',
+        'director': 'Woody Allen',
+        'year_created': 2008
+    },
+
+    {
+        'title': 'Manhattan',
+        'type': 'comedy',
+        'director': 'Woody Allen',
+        'year_created': 1979
+    },
+
+    {
+        'title': 'Psycho',
+        'type': 'thriller',
+        'director': 'Alfred Hitchcock',
+        'year_created': 1960
+    },
+
+    {
+        'title': 'The Birds',
+        'type': 'thriller',
+        'director': 'Alfred Hitchcock',
+        'year_created': 1963
+    },
+
+    {
+        'title': 'Det sjunde inseglet',
+        'type': 'drama',
+        'director': 'Ingmar Bergman',
+        'year_created': 1957
+    },
+
+    {
+        'title': 'Lincoln',
+        'type': 'biographical',
+        'director': 'Steven Spielberg',
+        'year_created': 2012
+    },
+
+    {
+        'title': 'Jurassic Park',
+        'type': 'Sci-Fi',
+        'director': 'Steven Spielberg',
+        'year_created': 1993
+    },
+
+    {
+        'title': 'Saving Private Ryan',
+        'type': 'drama',
+        'director': 'Steven Spielberg',
+        'year_created': 1998
+    },
+
+    {
+        'title': 'E.T.: The Extra-Terrestrial',
+        'type': 'Sci-Fi',
+        'director': 'Steven Spielberg',
+        'year_created': 1982
+    },
+
+    {
+        'title': 'Raiders of the Lost Ark',
+        'type': 'adventure',
+        'director': 'Steven Spielberg',
+        'year_created': 1981
+    },
+
+    {
+        'title': 'Artificial Intelligence: AI',
+        'type': 'drama',
+        'director': 'Steven Spielberg',
+        'year_created': 2001
+    },
+
+    {
+        'title': 'A Clockwork Orange',
+        'type': 'Sci-Fi',
+        'director': 'Stanley Kubrick',
+        'year_created': 1971
+    },
+
+    {
+        'title': '2001: A Space Odyssey',
+        'type': '	Sci-Fi',
+        'director': 'Stanley Kubrick',
+        'year_created': 1968
+    },
+
+    {
+        'title': 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+        'type': 'satire',
+        'director': 'Stanley Kubrick',
+        'year_created': 1964
+    },
+
+    {
+        'title': 'The Great Dictator',
+        'type': 'drama',
+        'director': 'Charles Chaplin',
+        'year_created': 1940
+    },
+
+    {
+        'title': 'Limelight',
+        'type': 'drama',
+        'director': 'Charles Chaplin',
+        'year_created': 1952
+    },
+
+    {
+        'title': 'Django Unchained',
+        'type': 'Western',
+        'director': 'Quentin Tarantino',
+        'year_created': 2012
+    },
+]
+
+
+
